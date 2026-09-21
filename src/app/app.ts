af903b8aa1728bd85aea3183518d75822e5519fa
ARSHEED ALAM <arsheedalam@ARSHEEDs-MacBook-Air.local>
2026-09-21 09:07:07 +0530

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
 import { Component} from '@angular/core';
-import {ViewChildComponent } from './viewChildrenComponent/view-child';
 @Component({
-  imports:[ViewChildComponent],
+  imports:[],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
